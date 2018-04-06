9487628698047f2b9a5bb56cce1debe9280597b7
League <League@AdminisorsiMac2.attlocal.net>
2018-04-06 14:42:03 -0700

    Day 5

--- src/BK.java
+++ src/BK.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class BK {
 public static void main(String[] args) {
 	
-
+//change
 	String input = JOptionPane.showInputDialog("What is the first letter a word?");
 String output = JOptionPane.showInputDialog("What is the second letter a word?");
 String put = JOptionPane.showInputDialog("What is the third letter of name, if you have one?");
